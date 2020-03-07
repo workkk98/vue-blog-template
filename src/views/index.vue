12370e6904fe463c454a2d903cb057d9906e9e33
workINgithub <853625072@qq.com>
2020-03-07 22:03:13 +0800

    fix: 修复第一次载入页面是空页面的bug

--- src/views/index.vue
+++ src/views/index.vue
@@ -13,6 +13,16 @@ export default {
       markdown: ''
     }
   },
+  beforeRouteEnter (to, from, next) {
+    // 对于第一次进入该页面的bug修复
+    next((vue) => {
+      if (vue.markdown === '') {
+        mdRoute.getComponent(to.path).then((md) => {
+          vue.markdown = md.default
+        })
+      }
+    })
+  },
   beforeRouteUpdate (to, from, next) {
     mdRoute.getComponent(to.path).then((md) => {
       this.markdown = md.default
